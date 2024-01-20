78a3ae3909f504405159348842218b9b86d4c6e8
Mohamedryouk <mohammedabdullah.ism@gmail.com>
2024-01-20 00:06:54 -0800

    done

--- 0-bubble_sort.c
+++ 0-bubble_sort.c
@@ -2,48 +2,55 @@
 #include <stdbool.h>
 #include "sort.h"
 /**
- *swap - swap function to swap between two addresses 
+ *swap - swap function to swap between two addresses
  *@xp: pointer integer to be sawped
  *@yp: pointer integer
  */
 void swap(int *xp, int *yp)
 {
-    int temp;
+	int temp;
 
-    temp = *xp;
-    *xp = *yp;
-    *yp = temp;
+	temp = *xp;
+	*xp = *yp;
+	*yp = temp;
 }
 /**
  *bubble_sort- bubble sort algorithem function
  *@array: array of numbers to be sorted.
  *@size: size of array to be sorted
  */
-void bubble_sort(int *array, size_t size) {
-    int swapped;
-    size_t i, j, k;
+void bubble_sort(int *array, size_t size)
+{
+	int swapped;
+	size_t i, j, k;
 
-    for (i = 0; i < size - 1; i++) {
-        swapped = 0;
+	for (i = 0; i < size - 1; i++)
+	{
+		swapped = 0;
 
-        for (j = 0; j < size - i - 1; j++) {
-            if (array[j] > array[j + 1]) {
-                swap(&array[j], &array[j + 1]);
-                swapped = 1;
-            }
-        }
+		for (j = 0; j < size - i - 1; j++)
+		{
+			if (array[j] > array[j + 1])
+			{
+				swap(&array[j], &array[j + 1]);
+				swapped = 1;
+			}
+		}
 
- 
-        for (k = 0; k < size; k++) {
-            printf("%d", array[k]);
-            if (k < size - 1) {
-                printf(", ");
-            }
-        }
-        printf("\n");
 
-        if (swapped == 0) {
-            break;
-        }
-    }
+		for (k = 0; k < size; k++)
+		{
+			printf("%d", array[k]);
+			if (k < size - 1)
+			{
+				printf(", ");
+			}
+		}
+		printf("\n");
+
+		if (swapped == 0)
+		{
+			break;
+		}
+	}
 }
--- print_array.c
+++ print_array.c
@@ -4,15 +4,18 @@
  *@array: array to be printed
  *@n: n number of elements
  */
-void print_array(int *array, size_t n) {
-    size_t i;
+void print_array(int *array, size_t n)
+{
+	size_t i;
 
-    printf(" ");
-    for (i = 0; i < n; i++) {
-        printf("%d", array[i]);
-        if (i < n - 1) {
-            printf(", ");
-        }
-    }
-    printf(" \n");
+	printf(" ");
+	for (i = 0; i < n; i++)
+	{
+		printf("%d", array[i]);
+		if (i < n - 1)
+		{
+			printf(", ");
+		}
+	}
+	printf(" \n");
 }
