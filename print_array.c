0d86c5d2b65c2093c1b3ea210acfb51314f43b2a
root <root@DESKTOP-S8OJ033.>
2024-01-20 10:18:54 +0200

    done

--- print_array.c
+++ print_array.c
@@ -1,21 +1,23 @@
-#include "sort.h"
-/**
- *print_array - to print array after every swap
- *@array: array to be printed
- *@n: n number of elements
- */
-void print_array(int *array, size_t n)
-{
-	size_t i;
+#include <stdlib.h>
+#include <stdio.h>
 
-	printf(" ");
-	for (i = 0; i < n; i++)
-	{
-		printf("%d", array[i]);
-		if (i < n - 1)
-		{
-			printf(", ");
-		}
-	}
-	printf(" \n");
+/**
+ * print_array - Prints an array of integers
+ *
+ * @array: The array to be printed
+ * @size: Number of elements in @array
+ */
+void print_array(const int *array, size_t size)
+{
+    size_t i;
+
+    i = 0;
+    while (array && i < size)
+    {
+        if (i > 0)
+            printf(", ");
+        printf("%d", array[i]);
+        ++i;
+    }
+    printf("\n");
 }
--- sort.h
+++ sort.h
@@ -2,7 +2,21 @@
 #define __SORT__
 #include <stdio.h>
 #include <stdbool.h>
-void print_array(int *array, size_t n);
+/**
+ * struct listint_s - Doubly linked list node
+ *
+ * @n: Integer stored in the node
+ * @prev: Pointer to the previous element of the list
+ * @next: Pointer to the next element of the list
+ */
+typedef struct listint_s
+{
+    const int n;
+    struct listint_s *prev;
+    struct listint_s *next;
+} listint_t;
+void print_array(const int *array, size_t size);
 void swap(int *xp, int *yp);
 void bubble_sort(int *array, size_t size);
+void print_list(const listint_t *list);
 #endif
