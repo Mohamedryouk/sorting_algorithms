9da2d1e952fc5dc7cb555cb2a3c076065435284e
root <root@DESKTOP-S8OJ033.>
2024-01-20 09:42:41 +0200

    done

---
+++ print_array.c
@@ -0,0 +1,18 @@
+#include "sort.h"
+/**
+ *print_array - to print array after every swap
+ *@array: array to be printed
+ *@n: n number of elements
+ */
+void print_array(int *array, size_t n) {
+    size_t i;
+
+    printf(" ");
+    for (i = 0; i < n; i++) {
+        printf("%d", array[i]);
+        if (i < n - 1) {
+            printf(", ");
+        }
+    }
+    printf(" \n");
+}
---
+++ sort.h
@@ -0,0 +1,8 @@
+#ifndef __SORT__
+#define __SORT__
+#include <stdio.h>
+#include <stdbool.h>
+void print_array(int *array, size_t n);
+void swap(int *xp, int *yp);
+void bubble_sort(int *array, size_t size);
+#endif
