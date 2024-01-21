622a47b92f95f87249a264219398e824c10d7eef
Mohamedryouk <mohammedabdullah.ism@gmail.com>
2024-01-21 06:13:04 -0800

    done

--- sort.h
+++ sort.h
@@ -11,9 +11,9 @@
  */
 typedef struct listint_s
 {
-    const int n;
-    struct listint_s *prev;
-    struct listint_s *next;
+	const int n;
+	struct listint_s *prev;
+	struct listint_s *next;
 } listint_t;
 void swap_nodes(listint_t **h, listint_t **data1, listint_t *data2);
 void print_array(const int *array, size_t size);
