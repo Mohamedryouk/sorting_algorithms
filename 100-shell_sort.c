69ee74772ca8bafefbdd950e343a7556b436c8ad
root <root@DESKTOP-S8OJ033.>
2024-01-23 14:48:52 +0200

    done

--- 100-shell_sort.c
+++ 100-shell_sort.c
@@ -4,30 +4,31 @@
  *@array: array to be sorted
  *@length: length of array
  */
-void shell_sort(int *array, size_t length)
+void shell_sort(int *array, size_t size)
 {
 	size_t outer, inner, gap = 1;
 	int insertion_value;
 
-	while( gap < length / 3)
+	while (gap <= size / 3)
 	{
 		gap = gap * 3 + 1;
 	}
-	while(gap > 0)
+	while (gap > 0)
 	{
-		for (outer = gap; outer < length; outer++)
+		for (outer = gap; outer < size; outer++)
 		{
-        		insertion_value = array[outer];
+			insertion_value = array[outer];
 			inner = outer;
 
-        		while(inner >= gap && array[inner - gap] > insertion_value)
-        		{
-        			array[inner] = array[inner - gap];
-        			inner -= gap;
-        		}
-        		array[inner] = insertion_value;
-        	}
-        	print_array(array, length);
-        	gap = (gap - 1) / 3;
-    	}
+			while(inner >= gap && array[inner - gap] > insertion_value)
+			{
+				array[inner] = array[inner - gap];
+				inner -= gap;
+			}
+			array[inner] = insertion_value;
+		}
+		print_array(array, size);
+		gap = (gap - 1) / 3;
+	}
 }
+	
