86f623cbce582a854beec5b3e19e10fe537b4857
root <root@DESKTOP-S8OJ033.>
2024-01-24 11:30:07 +0200

    done

--- 103-merge_sort.c
+++ 103-merge_sort.c
@@ -14,19 +14,19 @@ void print_subarray(const char *label, const int *array, size_t size)
  * merge - merge the arrays
  * @array: the array
  * @left: left side
- * @left_size: left length or size
+ * @L_size: left length or size
  * @right: right side
- * @right_size: right lenght or size
+ * @R_size: right lenght or size
  */
-void merge(int *array, int *left, size_t left_size, int *right, size_t right_size)
+void merge(int *array, int *left, size_t L_size, int *right, size_t R_size)
 {
 	size_t i = 0, j = 0, k = 0;
 
 	printf("Merging...\n");
-	print_subarray("left", left, left_size);
-	print_subarray("right", right, right_size);
+	print_subarray("left", left, L_size);
+	print_subarray("right", right, R_size);
 
-	while (i < left_size && j < right_size)
+	while (i < L_size && j < R_size)
 	{
 		if (left[i] <= right[j])
 		{
@@ -41,20 +41,20 @@ void merge(int *array, int *left, size_t left_size, int *right, size_t right_siz
 		k++;
 	}
 
-	while (i < left_size)
+	while (i < L_size)
 	{
 		array[k] = left[i];
 		i++;
 		k++;
 	}
 
-	while (j < right_size)
+	while (j < R_size)
 	{
 		array[k] = right[j];
 		j++;
 		k++;
 	}
-	print_subarray("Done", array, left_size + right_size);
+	print_subarray("Done", array, L_size + R_size);
 }
 /**
  * merge_sort - merge sort algorithm using top down
@@ -68,25 +68,25 @@ void merge_sort(int *array, size_t size)
 	if (size > 1)
 	{
 		size_t middle = size / 2;
-		size_t left_size = middle;
-		size_t right_size = size - middle;
+		size_t L_size = middle;
+		size_t R_size = size - middle;
 
-		int *left = malloc(left_size * sizeof(int));
-		int *right = malloc(right_size * sizeof(int));
+		int *left = malloc(L_size * sizeof(int));
+		int *right = malloc(R_size * sizeof(int));
 
-		for (i = 0; i < left_size; i++)
+		for (i = 0; i < L_size; i++)
 		{
 			left[i] = array[i];
 		}
 
-		for (i = 0; i < right_size; i++)
+		for (i = 0; i < R_size; i++)
 		{
 			right[i] = array[middle + i];
 		}
 
-		merge_sort(left, left_size);
-		merge_sort(right, right_size);
-		merge(array, left, left_size, right, right_size);
+		merge_sort(left, L_size);
+		merge_sort(right, R_size);
+		merge(array, left, L_size, right, R_size);
 
 		free(left);
 		free(right);
